2854024a6cdddb1ada0924ad120c5642044f89fb
Bruno <brunoviniciuspaese@gmail.com>
2026-01-06 22:23:53 -0300

    feat(cash-flow): adiciona fluxo de insercao do produto

--- pay-flow/src/components/ProductListCard/index.tsx
+++ pay-flow/src/components/ProductListCard/index.tsx
@@ -5,15 +5,85 @@ import Table, { type Column } from "../Table";
 import type { ProductFormData } from "../ProductCard";
 import Card from "../Card";
 import { useNavigate } from "react-router-dom";
+import Input from "../Input";
+import { formatCurrency } from "../../utils/formatCurrency";
+import Button from "../Button";
+import { Trash } from "lucide-react";
+import { useProductList } from "../../contexts/ProductList/useProductList";
+import { useCurrency } from "../../contexts/Currency/useCurrency";
 
-export interface ProductListCardProps {
-  columns: Column<ProductFormData>[];
-  data: ProductFormData[];
-}
-
-function ProductListCard({ columns, data }: ProductListCardProps) {
+function ProductListCard() {
   const { t } = useTranslation();
+  const { currency, locale } = useCurrency();
+  const { productList, removeProduct } = useProductList();
+
   const navigate = useNavigate();
+
+  const handleRemove = (product: ProductFormData) => {
+    removeProduct(product);
+  };
+
+  const columns: Column<ProductFormData>[] = [
+    {
+      key: "item",
+      label: t("checkout.product"),
+      align: "center",
+      width: "15%",
+    },
+    {
+      key: "description",
+      label: t("checkout.description"),
+      align: "center",
+      width: "37%",
+    },
+    {
+      key: "quantity",
+      label: t("checkout.quantity"),
+      align: "center",
+      width: "10%",
+      render: (value) => (
+        <Input
+          placeholder="aa"
+          text="aa"
+          // type="number"
+          value={value}
+          readOnly
+          center
+        />
+      ),
+    },
+    {
+      key: "unitPrice",
+      label: t("checkout.unitPrice"),
+      align: "center",
+      width: "15%",
+      render: (value, row) => {
+        return `${formatCurrency(Number(value), locale, currency)} (${
+          row.quantity
+        }x)`;
+      },
+    },
+    {
+      key: "price",
+      label: t("checkout.price"),
+      align: "center",
+      width: "15%",
+      render: (value) => formatCurrency(Number(value), locale, currency),
+    },
+    {
+      key: "item",
+      label: "",
+      align: "center",
+      width: "8%",
+      render: (_, row) => (
+        <Button
+          onClick={() => handleRemove(row)}
+          text={"checkout.removeProduct"}
+          icon={Trash}
+        />
+      ),
+    },
+  ];
 
   return (
     <Card title={t("checkout.productList")}>
@@ -21,7 +91,7 @@ function ProductListCard({ columns, data }: ProductListCardProps) {
         <Col>
           <Table<ProductFormData>
             columns={columns}
-            data={data}
+            data={productList}
             pageSize={10}
             onRowClick={(row) => navigate(`/checkout/product/${row.item}`)}
           />
--- pay-flow/src/pages/Checkout/index.tsx
+++ pay-flow/src/pages/Checkout/index.tsx
@@ -1,35 +1,19 @@
-import { useTranslation } from "react-i18next";
 import { useTheme } from "../../contexts/Theme/useTheme";
 import { Page } from "../Login/style";
 import HeaderControls from "../../components/HeaderControls";
 import { Body } from "./style";
 import { Row } from "../../components/Row";
 import { Col } from "../../components/Col";
-import ProductCard, {
-  type ProductFormData,
-} from "../../components/ProductCard";
+import ProductCard from "../../components/ProductCard";
 import ProductListCard from "../../components/ProductListCard";
 import DiscountCard from "../../components/DiscountCard";
 import CashierCard from "../../components/CashierCard";
-import { formatCurrency } from "../../utils/formatCurrency";
-import Input from "../../components/Input";
-import { useCurrency } from "../../contexts/Currency/useCurrency";
-import Button from "../../components/Button";
-import { Trash } from "lucide-react";
 import CustomerCard from "../../components/CustomerCard";
 import PaymentCard from "../../components/PaymentCard";
 import ShippingCard from "../../components/ShippingCard";
-import { useProductList } from "../../contexts/ProductList/useProductList";
 
 function Checkout() {
-  const { t } = useTranslation();
   const { theme } = useTheme();
-  const { currency, locale } = useCurrency();
-  const { productList, removeProduct } = useProductList();
-
-  const handleRemove = (product: ProductFormData) => {
-    removeProduct(product);
-  };
 
   return (
     <Page theme={theme}>
@@ -43,73 +27,7 @@ function Checkout() {
               <ProductCard />
             </Row>
             <Row>
-              <ProductListCard
-                columns={[
-                  {
-                    key: "item",
-                    label: t("checkout.product"),
-                    align: "center",
-                    width: "15%",
-                  },
-                  {
-                    key: "description",
-                    label: t("checkout.description"),
-                    align: "center",
-                    width: "37%",
-                  },
-                  {
-                    key: "quantity",
-                    label: t("checkout.quantity"),
-                    align: "center",
-                    width: "10%",
-                    render: (value) => (
-                      <Input
-                        placeholder="aa"
-                        text="aa"
-                        // type="number"
-                        value={value}
-                        readOnly
-                        center
-                      />
-                    ),
-                  },
-                  {
-                    key: "unitPrice",
-                    label: t("checkout.unitPrice"),
-                    align: "center",
-                    width: "15%",
-                    render: (value, row) => {
-                      return `${formatCurrency(
-                        Number(value),
-                        locale,
-                        currency
-                      )} (${row.quantity}x)`;
-                    },
-                  },
-                  {
-                    key: "price",
-                    label: t("checkout.price"),
-                    align: "center",
-                    width: "15%",
-                    render: (value) =>
-                      formatCurrency(Number(value), locale, currency),
-                  },
-                  {
-                    key: "item",
-                    label: "",
-                    align: "center",
-                    width: "8%",
-                    render: (_, row) => (
-                      <Button
-                        onClick={() => handleRemove(row)}
-                        text={"checkout.removeProduct"}
-                        icon={Trash}
-                      ></Button>
-                    ),
-                  },
-                ]}
-                data={productList}
-              />
+              <ProductListCard />
             </Row>
           </Col>
           <Col lg={3}>
