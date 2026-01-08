0d6dcb2abb6a9f173f8723fe1d58fd152b6434d2
Bruno <brunoviniciuspaese@gmail.com>
2026-01-07 22:05:36 -0300

    feat(cash-flow): implementa pagina metodo pagamento

--- pay-flow/src/components/Select/index.tsx
+++ pay-flow/src/components/Select/index.tsx
@@ -1,15 +1,22 @@
 import { useTheme } from "../../contexts/Theme/useTheme";
-import { Arrow, Container, StyledSelect } from "./style";
+import { Arrow, Container, StyledLabel, StyledSelect } from "./style";
 import type { Options } from "./type";
 
 interface SelectProps<T>
   extends Omit<React.SelectHTMLAttributes<HTMLSelectElement>, "onChange"> {
   options: Options<T>[];
+  label?: string;
   text: string;
   onChange?: (value: T) => void;
 }
 
-function Select<T>({ options, text, onChange, ...props }: SelectProps<T>) {
+function Select<T>({
+  options,
+  label,
+  text,
+  onChange,
+  ...props
+}: SelectProps<T>) {
   const { theme } = useTheme();
 
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
@@ -22,20 +29,23 @@ function Select<T>({ options, text, onChange, ...props }: SelectProps<T>) {
   };
 
   return (
-    <Container>
-      <StyledSelect theme={theme} onChange={handleChange} {...props}>
-        {options.map((option) => (
-          <option
-            key={String(option.value)}
-            value={String(option.value)}
-            title={text}
-          >
-            {option.label}
-          </option>
-        ))}
-      </StyledSelect>
-      <Arrow theme={theme} />
-    </Container>
+    <>
+      {label && <StyledLabel theme={theme}>{label}</StyledLabel>}
+      <Container>
+        <StyledSelect theme={theme} onChange={handleChange} {...props}>
+          {options.map((option) => (
+            <option
+              key={String(option.value)}
+              value={String(option.value)}
+              title={text}
+            >
+              {option.label}
+            </option>
+          ))}
+        </StyledSelect>
+        <Arrow theme={theme} />
+      </Container>
+    </>
   );
 }
 
